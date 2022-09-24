9a0e7033653158f41b2e3fd4d18ce7334dac0b49
Bing <tarzan@epic.com>
2022-09-24 13:28:21 +0800

    print source ip

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const express = require('express')
+
 const app = express()
+
+app.use(
+    express.urlencoded({
+      extended: true,
+    })
+  );
+  
+  app.use(express.json());
+
+
 const port = process.env.PORT||3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    var ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
+    console.log(ip) 
+
+
+  res.send('Hello World~~!')
 })
 
 app.listen(port, () => {
